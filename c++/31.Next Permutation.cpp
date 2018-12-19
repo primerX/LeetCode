4f670097011913f1d9869fd5f4c0335c85ec4d3d
primerX <1034805730@qq.com>
2018-12-19 09:34:57 +0800

    Update 31.Next Permutation.cpp

--- c++/31.Next Permutation.cpp
+++ c++/31.Next Permutation.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        if(nums.size() <= 1) return;
+
+        for(int i = nums.size()-1; i > 0; i--){
+        	if(nums[i-1] < nums[i]){
+        		int j = nums.size() - 1;
+        		while(nums[i-1] >= nums[j]){
+        			j--;
+        		}
+        		swap(nums[i-1], nums[j]);
+        		reverse(nums.begin() + i, nums.end());
+        		return;
+        	}
+        	// 4 3 2 1 的情况
+        	if(i == 1){
+        		reverse(nums.begin(), nums.end());
+        		return;
+        	}
+        }
     }
 };
