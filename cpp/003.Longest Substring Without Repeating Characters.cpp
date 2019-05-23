8147491c320af7a02b6f872430b8b45f13053010
primerX <1034805730@qq.com>
2019-05-23 23:22:19 +0800

    Update 003.Longest Substring Without Repeating Characters.cpp

--- cpp/003.Longest Substring Without Repeating Characters.cpp
+++ cpp/003.Longest Substring Without Repeating Characters.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxLen = 0, lastTRepeatPos = -1;
+    	// 最长子串的长度，	最后一个重复字符的位置
+        int maxLen = 0, lastRepeatPos = -1;
+
+        // mp 用来存储字符与位置之间的关系
         unordered_map<char, int> mp;
 
         for(int i = 0; i < s.length(); i++){
-        	if(mp.find(s[i]) != mp.end() && lastTRepeatPos < mp[s[i]]){
-        		lastTRepeatPos = mp[s[i]];
+        	// 更新最后一个重复字符的位置，后面一条语句是为了防止 "abba" 这种情况
+        	if(mp.find(s[i]) != mp.end() && lastRepeatPos < mp[s[i]]){
+        		lastRepeatPos = mp[s[i]];
         	}
-        	if(i - lastTRepeatPos > maxLen){
-        		maxLen = i - lastTRepeatPos;
+        	// 更新最长子串的长度
+        	if(i - lastRepeatPos > maxLen){
+        		maxLen = i - lastRepeatPos;
         	}
         	mp[s[i]] = i;
         }
