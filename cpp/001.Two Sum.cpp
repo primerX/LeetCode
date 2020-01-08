f13c20901bdaac0a3215ee157c2d127fe6d7ae64
primerX <1034805730@qq.com>
2020-01-08 23:31:24 +0800

    Update 001.Two Sum.cpp

--- cpp/001.Two Sum.cpp
+++ cpp/001.Two Sum.cpp
@@ -1,21 +1,17 @@
 class Solution{
 public:
-	vector<int> twoSum(vector<int> nums, int target){
-		vector<int> result;
-		// map 中的 key 放入 target - nums[i];
-		// map 中的 value 放入所需要这个数的下标
-		// 因此，当我们检查下一个 nums[i] 时，如果 nums[i] 存在于 map 中
-		// 意味着我们找到了第二个数
+	vector<int> twoSum(vector<int>& nums, int target){
 		unordered_map<int, int> mp;
+		vector<int> result;
 		for(int i = 0; i < nums.size(); i++){
-			if(mp.find(nums[i]) == mp.end()){	// 如果没有找到
-				mp[target - nums[i]] = i;
-			}else{		// 在 mp 中找到需要的数
-				result.push_back(mp[nums[i]]);
+			if(mp.find(target - nums[i]) == mp.end()){
+				mp[nums[i]] = i;
+			}else{
+				result.push_back(mp[target - nums[i]]);
 				result.push_back(i);
-				break;
 			}
 		}
+
 		return result;
 	}
 };
