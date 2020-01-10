38e3364dd540ca3e615950ccd53597e6b76495c5
primerX <1034805730@qq.com>
2020-01-10 22:22:34 +0800

    Update 019.Remove Nth Node From End of List.cpp

--- cpp/019.Remove Nth Node From End of List.cpp
+++ cpp/019.Remove Nth Node From End of List.cpp
@@ -9,23 +9,23 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head == NULL || n < 1){
-        	return head;
+        if(head == NULL){
+            return head;
         }
         ListNode *cur = head;
-        while(cut != NULL){
-        	n--;
-        	cur = cut->next;
+        while(cur != NULL){
+            n--;
+            cur = cur->next;
         }
-        if(n == 0){			// 链表长度刚好等于 n
-        	head = head->next;
+        if(n == 0){         // 链表长度刚好等于 n
+            head = head->next;
         }
         if(n < 0){
-        	cur = head;
-        	while(++n != 0){
-        		cur = cur->next;
-        	}
-        	cur->next = cur->next->next;
+            cur = head;
+            while(++n != 0){
+                cur = cur->next;
+            }
+            cur->next = cur->next->next;
         }
         return head;
     }
