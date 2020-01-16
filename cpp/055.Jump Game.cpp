1cee742ee056237686fb7f603f0106e675b091c4
primerX <1034805730@qq.com>
2020-01-16 23:10:15 +0800

    update

--- cpp/055.Jump Game.cpp
+++ cpp/055.Jump Game.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-    	// distance 为能移动的最大距离
-        int distance = 0;
-        for(int i = 0; i < nums.size() && i <= distance; i++){
-        	distance = max(distance, i + nums[i]);
+    	// max_distance 为能移动的最大距离
+        int max_distance = 0;
+        for(int i = 0; i < nums.size() && i <= max_distance; i++){
+        	max_distance = max(max_distance, i + nums[i]);
         }
-        return distance >= (nums.size() - 1);
+        return max_distance >= (nums.size() - 1);
     }
 };
