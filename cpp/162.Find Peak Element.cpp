31d8c05b320073322a6a4bbb5db31c0b3ed58d44
primerX <1034805730@qq.com>
2020-02-07 14:45:07 +0800

    Update 162.Find Peak Element.cpp

--- cpp/162.Find Peak Element.cpp
+++ cpp/162.Find Peak Element.cpp
@@ -5,16 +5,21 @@ public:
         int mid = 0;
 
         while(low < high){
-        	mid = low + (high - low) / 2;
-        	if(( mid == 0 || nums[mid] > nums[mid-1] )  && nums[mid] > nums[mid+1]){
-        		return mid;
-        	}
-
-        	if(nums[mid] < nums[mid-1] && mid > 0){
-        		high = mid - 1;
-        	}else{
-        		low = mid + 1;
-        	}
+              // Find the index of middle element
+            mid = low + (high - low) / 2;
+            // Compare middle element with its neighbours (if neighbours exist)
+            if((mid == 0 || nums[mid] > nums[mid-1]) && (mid == nums.size()-1 || nums[mid] > nums[mid+1])){
+                return mid;
+            }
+            // If middle element is not peak and its left neighbor is greater than it
+            // then left half must have a peak element
+            if(mid >0 && nums[mid-1] > nums[mid]){
+                high = mid - 1;
+            // If middle element is not peak and its right neighbor is greater than it
+            // then right half must have a peak element
+            }else{
+                low = mid + 1;
+            }
         }
         return low;
     }
