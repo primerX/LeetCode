d65e8287f7c69f59b31e85efb0d8b0f819328f53
primerX <1034805730@qq.com>
2020-02-08 13:47:22 +0800

    Update 191.Number of 1 Bits.cpp

--- cpp/191.Number of 1 Bits.cpp
+++ cpp/191.Number of 1 Bits.cpp
@@ -8,3 +8,18 @@ public:
         return cnt;
     }
 };
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int cnt = 0;
+        // for(; n > 0; n /= 2){
+        // 	if(n & 0x1) cnt++;
+        // }
+        while(n != 0){
+        	if(n & 0x1) cnt++;
+        	n /= 2;
+        }
+        return cnt;
+    }
+};
