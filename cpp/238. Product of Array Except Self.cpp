886ca3700ef0560876ece5d513d3bb58c9dfb0d1
primerX <1034805730@qq.com>
2020-02-10 13:49:35 +0800

    Create 238. Product of Array Except Self.cpp

---
+++ cpp/238. Product of Array Except Self.cpp
@@ -0,0 +1,21 @@
+class Solution{
+public:
+	vector<int> productExceptSelf(vector<int> &nums){
+		int len = nums.size();
+		vector<int> result(len, 1);
+
+		// from the left to right
+		for(int i = 1; i < len; i++){
+			result[i] = result[i-1] * nums[i-1];
+		}
+
+		// from the right to left
+		int factorial = 1;
+		for(int i = len-2; i >= 0; i--){
+			factorial *= nums[i+1];
+			result[i] *= factorial;
+		}
+		return result;
+	}
+	
+};
